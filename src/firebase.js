e91f5c566da313be52b68a3f0a898f1fc0bc13b0
Dhruv Gupta <109941217+Dhruv3110@users.noreply.github.com>
2025-07-17 18:10:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPMhRRErfb3B77Wg3K9OGIG3tuVLBs3jo",
-  authDomain: "my-project-fd48e.firebaseapp.com",
-  databaseURL: "https://my-project-fd48e-default-rtdb.firebaseio.com",
-  projectId: "my-project-fd48e",
-  storageBucket: "my-project-fd48e.firebasestorage.app",
-  messagingSenderId: "176880148876",
-  appId: "1:176880148876:web:40b7158dfbb9bc754d76a9",
-  measurementId: "G-WR0TFKNMGJ"
+// Add your firebaseConfig details
 };
 
 // Initialize Firebase
